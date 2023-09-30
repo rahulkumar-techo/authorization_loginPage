da116c518dae6011934c5894c845116a5e505885
rahul kumar <rahulkumar9142684664@gmail.com>
2023-09-30 17:53:52 +0530

    created hashed password

--- src/controllers/registerCon.js
+++ src/controllers/registerCon.js
@@ -1,5 +1,5 @@
 import User from "../models/userSchema.js";
-
+import bcrypt from "bcryptjs";
 
 const registerCon = async (req, res) => {
   if (!req.body)
@@ -9,7 +9,7 @@ const registerCon = async (req, res) => {
 
   try {
     const user = await User.findOne({ email });
-    if (user) return res.status(409).json({ message: 'User Already Exists' });
+    if (user) return res.status(409).json({ message: "User Already Exists" });
 
     if (!name || !email || !phone || !password || !cpassword)
       return res.status(400).json({ message: "Fill all inputs" });
@@ -17,13 +17,20 @@ const registerCon = async (req, res) => {
     if (password !== cpassword)
       return res.status(400).json({ message: "password should be Matched" });
 
-
-
-    const userData = await User.create({ name, email, phone, password,cpassword });
+  
+    const userData = await User.create({
+      name,
+      email,
+      phone,
+      password,
+      cpassword,
+    });
 
     res.status(200).json({ message: "saved at data base", userData });
   } catch (error) {
-    res.status(500).json({ message: `Something went wrong at register  ${error}` });
+    res
+      .status(500)
+      .json({ message: `Something went wrong at register  ${error}` });
   }
 };
 export default registerCon;
--- src/models/userSchema.js
+++ src/models/userSchema.js
@@ -1,34 +1,49 @@
-import mongoose,{Schema,model} from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
-const userSchema = new Schema({
-name:{
-    type:String,
-    required:true
-},
-email:{
-    type:String,
-    required:true,
-    unique:true
-},
-phone:{
-    type:Number,
-    required:true,
-    
-},
-work:{
-    type:String
-},
-password:{
-    type:String,
-    required:true
-},
-cpassword:{
-    type:String,
-    required:true
-}
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    work: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    cpassword: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  // isModified
+  // we need to hashed the Password , when someone change the password
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.cpassword =await bcrypt.hash(this.cpassword, 10);
+  }
+
+  next();
+});
 
 
-},{timestamps:true});
 
-const User = model("User",userSchema);
+const User = model("User", userSchema);
+
 export default User;
